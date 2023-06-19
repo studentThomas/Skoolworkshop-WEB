ff55b1816c0a41a9baa6f244b4aecb91a06bf448
studentThomas <twa.vermeulen@student.avans.nl>
2023-06-19 09:49:06 +0200

    Added edit and delete icons

--- app/workshops/[workshop]/page.tsx
+++ app/workshops/[workshop]/page.tsx
@@ -180,27 +180,36 @@ function Product({ product, params, deleteProduct, updateProduct }: any) {
           <img src={Image} className="card-img-top product-image" alt={Name} />
         </Link>
         <div className="card-body d-flex flex-column">
-          <h5 className="card-title mb-auto">{Name}</h5>
-          <div className="d-flex justify-content-between align-items-start mt-2">
-            <div className="d-flex gap-2">
-              <button
-                onClick={() => setShowModalUpdate(true)}
-                className="btn btn-sm btn-outline-secondary"
-              >
-                Update
-              </button>
-            </div>
-            <h5 className={`text-end ${Quantity <= 0 ? "text-danger" : ""}`}>{Quantity}</h5>
-            <div className="d-flex justify-content-end">
-              <button
-                onClick={() => setShowModal(true)}
-                className="btn btn-sm btn-outline-secondary"
-              >
-                Delete
-              </button>
-            </div>
-          </div>
-        </div>
+  <h5 className="card-title mb-auto">{Name}</h5>
+  <div className="d-flex justify-content-between align-items-starts">
+    <div className="d-flex gap-1 mt-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        fill="currentColor"
+        className="bi bi-pencil text-warning"
+        viewBox="0 0 16 16"
+        onClick={() => setShowModalUpdate(true)}
+      >
+        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        fill="currentColor"
+        className="bi bi-trash text-danger mx-1"
+        viewBox="0 0 16 16"
+        onClick={() => setShowModal(true)}
+      >
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+      </svg>
+    </div>
+    <h4 className={`text-end ${Quantity <= 0 ? "text-danger" : ""}`}>{Quantity}</h4>
+  </div>
+</div>
       </div>
     </div>
   );
